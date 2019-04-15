53f036ab245047fd83cae66fc862e5f374469568
Renata <vrenata8@gmail.com>
2019-04-15 17:16:04 +0200

    Validation of branches fixes
    Tested and fixed validation of branches. Updated comments and docs

--- taf/oll/validation.py
+++ taf/oll/validation.py
@@ -1,76 +1,61 @@
 import taf.oll.targetsdb as targetsdb
 from taf.oll.exceptions import InvalidBranch
 
-def validate_branch(auth_repo, branch_name):
+def validate_branch(auth_repo, target_repos, branch_name, default_branch='master'):
   """
-  This should check the following:
-  - If all commits on a speculative branch have the same date in their repository metadata.
-  - If commit sha of each commit of a speculative branch matches
-  value of that repo's target in targets.json.
-  - If versions of tuf metadata increases by one from one commit
+  Validates corresponding branches of the authentication repository
+  and the target repositories. Assumes that:
+  1. Commits of the target repositories' branches are merged into the default (master) branch
+  2. Commits of the authentication repository are not merged into the default (master) branch
+  directly - fresh timestamp and snapshot are generated.
+  Checks if:
+  1. For each target repository, a commit sha of each commit of the specified branch matches
+  the commit sha stored in the target file corresponding to that repository.
+  2. If versions of tuf metadata increases by one from one commit
   to the next commit of a speculative branch in the authentication repository
-  """
-  # for now create the target repositories based on the the HEAD targets.json
-  # assuming that all commits on a branch (including the branching commit)
-  # have the same target repositories defined in targets.json
-  # TODO what if a user wants to start protecting a new repository?
 
-  target_repos = targetsdb.get_targets(auth_repo)
+  # TODO Add speculative branch ID as a new target
+  # TODO Check if all commits of an authentication repository's speculative branch have the same branch ID
+  """
+
   targets_and_commits = {target_repo: target_repo.
                          commits_on_branch_and_not_other(branch_name, 'master')
                          for target_repo in target_repos}
 
   auth_commits = auth_repo.commits_on_branch_and_not_other(branch_name, 'master')
-  _check_lengths_of_branches(targets_and_commits)
-
+  _check_lengths_of_branches(targets_and_commits, branch_name)
   targets_version = None
-
-  # Commits of authentication repository's branches should not be merged into master directly
-  # in order to avoid merging timestamp and snapshot which could expire before a commit
-  # is merged. On the other hand, commit belonging to branches of speculative repositories
-  # are merged into master. This means that the number of commits not yet merged into master
-  # for a given branch should be the same for all target repositories. However, that
-  # number will, unless no commits have been merged yet, not correspond to the number
-  # of commits of the matching authentication repository's branch.
-  # Top n commits of the authentication repsitory's branch should correspond to the commits
-  # target repositories' commits.Commits are ordered from the one at the end of the branch
-  # to the branching commit
 
   # fill the shorter lists with None values, so that their sizes matches the sizes
   # of authentication repository's commits list
   for commits in targets_and_commits.values():
     commits.extend([None] * (len(auth_commits) - len(commits)))
 
-  for commit_index in range(auth_commits):
-    auth_commit = auth_commits[commit_index]
+  for commit_index, auth_commit in enumerate(auth_commits):
     # load content of targets.json
     targets = auth_repo.get_json(auth_commit, 'metadata/targets.json')
+    targets_version = _check_targets_version(targets, auth_commit, targets_version)
+
     for target, target_commits in targets_and_commits.items():
       target_commit = target_commits[commit_index]
 
-      # check if codified-xml and html commits match the target commits specified in the tuf repo
+      # targets' commits match the target commits specified in the authentication repository
       if target_commit is not None:
         _compare_commit_with_targets_metadata(auth_repo, auth_commit, target, target_commit)
         # _check_commits
 
-      targets_version = _check_targets_version(targets, auth_commit, targets_version)
+
 
 
 def _check_lengths_of_branches(targets_and_commits, branch_name):
   """
-   Speculative branches of -xml-codified and -html repositories should always have the same
-   length. Initially, authentication repo's speculative branch will have that same length
-   too. However, once the first set of commits is merged, this will change. Commits on
-   -xml-codified and -html repositories' speculative branches are merged into the master
-   branches of these repos, unlike authentication repository's speculative commits.
-   In case of the auth repo, new commits are created, with freshly generated snapshot
-   and timestamp. So, -xml-codified and -xml speculative branches will become shorter
-   after every merge.
-   """
+  Checks if branches bracnhes of the given name have the same number
+  of commits in each of the provided repositories.
+  """
 
   lengths = set(len(commits) for commits in targets_and_commits.values())
   if len(lengths) > 1:
-    msg = f'Branches {branch_name} of target repositories have a different number of commits'
+    msg = f'Branches {branch_name} of target repositories do not have the same number of commits'
     for target, commits in targets_and_commits.items():
       msg += f'\n{target.target_path} has {len(commits)} commits.'
     raise InvalidBranch(msg)
@@ -102,6 +87,6 @@ def _compare_commit_with_targets_metadata(tuf_repo, tuf_commit, target_repo, tar
   specified target value in targets.json.
   """
   targets_head_sha = tuf_repo.get_json(tuf_commit, f'targets/{target_repo.target_path}')['commit']
-  if repo_commit != targets_head_sha:
-    raise InvalidBranch(f'Commit {repo_commit} of repository {repo.name} does '
+  if target_repo_commit != targets_head_sha:
+    raise InvalidBranch(f'Commit {target_repo_commit} of repository {target_repo.name} does '
                         'not match the commit sha specified in targets.json!')
