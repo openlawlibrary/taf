c41eeef6d8658c2db5adf55afa18fc756b494ebb
Renata <vrenata8@gmail.com>
2019-05-27 15:57:19 +0200

    Update cli command fixes

--- taf/cli.py
+++ taf/cli.py
@@ -57,11 +57,11 @@ def update_metadata_expiration_date(repo_path, keystore_path, metadata_role, exp
 @click.option('--url', help="Authentication repository's url")
 @click.option('--clients-dir', help="Directory containing the client's authentication repository")
 @click.option('--repo-name', help="Authentication repository's name")
-@click.option('--targets_dir', help="Directory containing the target repositories")
-@click.option('--from-filesystem', default=False, help='Indicates if the we want to clone a '
+@click.option('--targets-dir', help="Directory containing the target repositories")
+@click.option('--from-fs', is_flag=True, default=False, help='Indicates if the we want to clone a '
               'repository from the filesystem')
-def update(url, clients_dir, repo_name, targets_dir, from_filesystem):
-  taf_updater(url, clients_dir, repo_name, targets_dir, from_filesystem)
+def update(url, clients_dir, repo_name, targets_dir, from_fs):
+  taf_updater(url, clients_dir, repo_name, targets_dir, from_fs)
 
 
 cli()
