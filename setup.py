d172218a8a92a57ae28966baa3220a21099c6496
Daniel Elero <danixeee@gmail.com>
2019-05-30 18:47:03 +0200

    Add 'colorama>=0.3.9' dep

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'click==6.7',
+        'colorama>=0.3.9'
         'cryptography==2.3.1',
         'oll-sc==0.1.0',
         'oll-tuf==0.11.2.dev5',
