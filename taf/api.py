77f49fd2fbb6e2e8acfc5d38e905b703fad823de
Daniel Elero <danixeee@gmail.com>
2019-05-31 14:24:49 +0200

    Change API to use TUF key format

--- taf/api.py
+++ taf/api.py
@@ -1,20 +1,44 @@
-from securesystemslib.keys import _get_keyid
-
 from oll_sc.api import sc_export_pub_key_pem
+from tuf.repository_tool import import_rsakey_from_pem
 
 
-def sc_get_tuf_key_id(key_id, pin):
-  """Return key id used by TUF.
-  NOTE: This function uses `securesystemslib.keys._get_keyid` to hash smart
-        card's public key.
+def sc_get_public_key(key_slot, pin):
+  """Return public key from a smart card.
+
+  Args:
+    - key_slot(tuple): Key ID as tuple (e.g. (1,))
+    - pin(str): Pin for session login
+
+  Returns:
+    A dictionary containing the RSA keys and other identifying information
+    from inserted smart card.
+    Conforms to 'securesystemslib.formats.RSAKEY_SCHEMA'.
+
+  Raises:
+    - SmartCardNotPresentError: If smart card is not inserted
+    - SmartCardWrongPinError: If pin is incorrect
+    - SmartCardFindKeyObjectError: If public key for given key id does not exist
+    - securesystemslib.exceptions.FormatError: if 'PEM' is improperly formatted.
   """
-  pub_key_pem = sc_export_pub_key_pem(key_id, pin)
-  return _get_keyid('rsa', 'rsassa-pss-sha256', dict(
-      public=pub_key_pem.decode('utf-8').rstrip()
-  ))
+  pub_key_pem = sc_export_pub_key_pem(key_slot, pin).decode('utf-8')
+  return import_rsakey_from_pem(pub_key_pem)
 
 
-def check_inserted_key_id(repository, role, key_id, pin):
-  """Check if inserted smart card's private key id is same as TAF's key id."""
-  sc_key_id = sc_get_tuf_key_id(key_id, pin)
-  return sc_key_id in repository.get_role_keys(role)
+def check_inserted_key_id(repository, role, key_slot, pin):
+  """Return public key from a smart card.
+
+  Args:
+    - key_slot(tuple): Key ID as tuple (e.g. (1,))
+    - pin(str): Pin for session login
+
+  Returns:
+    Boolean. True if smart card key id belongs to metadata role key ids
+
+  Raises:
+    - SmartCardNotPresentError: If smart card is not inserted
+    - SmartCardWrongPinError: If pin is incorrect
+    - SmartCardFindKeyObjectError: If public key for given key id does not exist
+    - securesystemslib.exceptions.FormatError: if 'PEM' is improperly formatted.
+  """
+  public_key = sc_get_public_key(key_slot, pin)
+  return public_key['keyid'] in repository.get_role_keys(role)
