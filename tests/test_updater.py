d4ca1cd8c475b420b0a2d688d1fae7b169d8546a
Renata <vrenata8@gmail.com>
2019-06-19 13:07:00 +0200

    Fixed some typos

--- tests/test_updater.py
+++ tests/test_updater.py
@@ -1,39 +1,40 @@
 """
-This module includes test for the taf updater. The basic idea behind the tests is the following:
-1. For each situation we want to test there is a set of target repositories and an authentication repository.
+This module includes tests for the taf updater. The basic idea behind the tests is the following:
+1. For each situation we want to test, there is a set of target repositories and an authentication repository.
 For example, repositories where everything is completely valid, repositories where there is a mismatch between
-a target repository's commit and the corresponding target commit sha in the auth repo do not match etc.
+a target repository's commit and the corresponding target commit sha in the auth repo etc.
 These repositories are placed in tests/data/repos/test-updater directory and are copied to the origin directory
 before any of the tests are executed. The repositories in the origin directory are used as remotes.
 As a future imrpovements, we could reduce the number of repositories by adding/removing commits from tests.
 2. We test update when no client repositories exist and when they do exist, but are not up to date.
-When testing the second case, we clone the repositories and then revert them.
+When testing the second case, we clone the repositories and then revert them. We only modify client's repositories.
 3. When everything is set up, update is called. Afterwards:
   1) If the update is expected to be successful, it is checked if pulled commits match commits of the remote
    repositories and if last_successful commit was created.
   2) If the update is expected to be unsuccessful, it is checked if the client't repositories remained the same
-  (if they existed) or if they still do not exist (in case they didn't exist in the first place) and if the
+  (if they existed) or if they do not exist (in case they didn't exist in the first place) and if the
   error message is as expected.
-4. Client repositories are deleted after every test, to make sure that the execution of a previous test does
+4. Client repositories are deleted after every test to make sure that the execution of a previous test does
 not impact the following ones.
 
 These states of repositories are tested:
 1. The authentication and target repositories are valid and all of the commits match - test-updater-valid. This
-set of repositories is also used to test if the updater works in case when there is no need to perform the update.
+set of repositories is also used to test if the updater works in case when there is no need to perform the update,
+and if the update fails if there are no client target repositories, but the auth repo exists.
 2. There are additional commits in target repos (commits which are not accounted for by the auth repo) -
-test-updater-additional-target-commit The update is performed until the last validated commit. Updated does not
-raise an error (a warning is logged).
+test-updater-additional-target-commit The update is performed until the last commit that can be validated.
+Updated does not raise an error (a warning is logged).
 3. The authentication and target repositories are valid and all of the commits match, but there are also auth
 repo commits where only metadata files were updated (e.g. timestamp expiration date was changed) -
-test-updater-valid-with-updated-expiration-dates.
-4. There are more commits noted in auth repo target files that do not match commits of target repositries -
+test-updater-valid-with-updated-expiration-dates. The update should be successful.
+4. There are more commits noted in auth repo's target files than target repositries' commits
 test-updater-missing-target-commit. The update is expected to fail.
 5. The commits of the target repositories do not match commits noted in the auth repo - test-updater-invalid-target-sha.
 The update is expected to fail.
 6. A metadata file's signature is not valid - test-updater-wrong-key. The update is expected to fail.
 7. A metadata file's expiration date is not valid - test-updater-invalid-expiration-date.
 The update is expected to fail.
-8. A metadata file's version number is not valid (this tesst case was created by swapping commits using
+8. A metadata file's version number is not valid (this test case was created by swapping commits using
 git rebase) - test-updater-invalid-version-number. The update is expected to fail.
 9. A metadata file which should have remained the same changed (this test case was created by updating targets
 and not updating snaphost) - test-updater-just-targets-updated. The update is expected to fail.
