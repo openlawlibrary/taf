5b357350aefa4457c934911dc0b6e27bb24054e8
Daniel Elero <danixeee@gmail.com>
2019-08-26 14:50:17 +0200

    Load libusb

--- taf/__init__.py
+++ taf/__init__.py
@@ -2,8 +2,11 @@ import os
 import sys
 from pathlib import Path
 
-_YKPERS_PATH = str((Path(__file__).parent.parent / 'ykpers').resolve())
 _PLATFORM = sys.platform
+
+_EXT_LIBS = Path(__file__).parent.parent / 'ext_libs'
+_YKPERS_PATH = str((_EXT_LIBS / 'ykpers').resolve())
+_LIB_USB_PATH = str((_EXT_LIBS / 'libusb').resolve())
 
 
 def _set_env(env_name, path):
@@ -15,10 +18,13 @@ def _set_env(env_name, path):
 
 if _PLATFORM == 'darwin':
   _set_env('DYLD_LIBRARY_PATH', _YKPERS_PATH)
+  _set_env('DYLD_LIBRARY_PATH', _LIB_USB_PATH)
 elif _PLATFORM == 'linux':
   _set_env('LD_LIBRARY_PATH', _YKPERS_PATH)
+  _set_env('LD_LIBRARY_PATH', _LIB_USB_PATH)
 elif _PLATFORM == 'win32':
   _set_env('PATH', _YKPERS_PATH)
+  _set_env('PATH', _LIB_USB_PATH)
 else:
   raise Exception('Platform "{}" is not supported!'.format(_PLATFORM))
 
