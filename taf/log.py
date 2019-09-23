bc5f8ba7e1f14b0d9a76860a7be3252097c40de0
Renata <vrenata8@gmail.com>
2019-09-23 16:54:16 +0200

    Addressed code review

--- taf/log.py
+++ taf/log.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from pathlib import Path
 
 import taf.settings
@@ -10,6 +11,17 @@ formatter = logging.Formatter(_FORMAT_STRING)
 logger = logging.getLogger('taf')
 logger.setLevel(taf.settings.LOG_LEVEL)
 
+def _get_log_location():
+  lcation = taf.settings.LOGS_LOCATION
+  if lcation is None:
+    if os.name == 'nt':
+      lcation = Path.home() / '.taf'
+      lcation.mkdir(exist_ok=True)
+    else:
+      lcation = Path('/var/log')
+  else:
+    lcation = Path(logs_location)
+  return lcation
 
 
 if taf.settings.ENABLE_CONSOLE_LOGGING:
@@ -20,14 +32,7 @@ if taf.settings.ENABLE_CONSOLE_LOGGING:
 
 
 if taf.settings.ENABLE_FILE_LOGGING:
-  logs_location = taf.settings.LOGS_LOCATION
-  if logs_location is None:
-    logs_location = Path('/var/log')
-    if not logs_location.is_dir():
-      logs_location = Path.home() / '.taf'
-      logs_location.mkdir(exist_ok=True)
-  else:
-    logs_location = Path(logs_location)
+  logs_location = _get_log_location()
   file_handler = logging.FileHandler(str(logs_location / taf.settings.LOG_FILENAME))
   file_handler.setLevel(taf.settings.FILE_LOGGING_LEVEL)
   file_handler.setFormatter(formatter)
