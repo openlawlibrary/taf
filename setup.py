ec204ee0c3c912a3fc78866361bd022ec5e86fac
Daniel Elero <danixeee@gmail.com>
2019-09-25 16:34:27 +0200

    Bump version to 0.1.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 PACKAGE_NAME = 'taf'
-VERSION = '0.1.6'
+VERSION = '0.1.7'
 AUTHOR = 'Open Law Library'
 AUTHOR_EMAIL = 'info@openlawlib.org'
 DESCRIPTION = 'Implementation of archival authentication'
