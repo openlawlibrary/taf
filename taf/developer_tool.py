fd084788126e98790a94edf7122e561c15c81a47
Renata <vrenata8@gmail.com>
2020-01-28 15:31:51 +0100

    Formatting

--- taf/developer_tool.py
+++ taf/developer_tool.py
@@ -396,7 +396,9 @@ def create_repository(
 
     # if the repository is a test repository, add a target file called test-auth-repo
     if test:
-        test_auth_file = Path(repo.repo_path, repo.targets_path) / repo.TEST_REPO_FLAG_FILE
+        test_auth_file = (
+            Path(repo.repo_path, repo.targets_path) / repo.TEST_REPO_FLAG_FILE
+        )
         test_auth_file.touch()
         targets_obj = _role_obj("targets", repository)
         targets_obj.add_target(repo.TEST_REPO_FLAG_FILE)
