47c08c5508d1f5689eb8b8c2752ebb1dad026de5
Renata <vrenata8@gmail.com>
2020-03-02 15:55:10 +0100

    Formatting

--- taf/log.py
+++ taf/log.py
@@ -10,6 +10,7 @@ _FORMAT_STRING = "[{time}] [{level}] [{module} {function}@{line}]\n{message}\n"
 console_loggers = {}
 file_loggers = {}
 
+
 def _get_log_location():
     location = settings.LOGS_LOCATION or os.environ.get("TAF_LOG")
     if location is None:
@@ -23,10 +24,10 @@ def _get_log_location():
 taf_logger.remove()
 
 if settings.ENABLE_CONSOLE_LOGGING:
-    console_loggers['log'] = taf_logger.add(
+    console_loggers["log"] = taf_logger.add(
         sys.stdout, format=_FORMAT_STRING, level=settings.CONSOLE_LOGGING_LEVEL
     )
-    console_loggers['error'] = taf_logger.add(
+    console_loggers["error"] = taf_logger.add(
         sys.stderr, format=_FORMAT_STRING, level=settings.ERROR_LOGGING_LEVEL
     )
 
@@ -34,21 +35,19 @@ if settings.ENABLE_CONSOLE_LOGGING:
 if settings.ENABLE_FILE_LOGGING:
     logs_location = _get_log_location()
     log_path = str(logs_location / settings.LOG_FILENAME)
-    file_loggers['log'] = taf_logger.add(
+    file_loggers["log"] = taf_logger.add(
         log_path, format=_FORMAT_STRING, level=settings.FILE_LOGGING_LEVEL
     )
 
     if settings.SEPARATE_ERRORS:
         error_log_path = str(logs_location / settings.ERROR_LOG_FILENAME)
-        file_loggers['error'] = taf_logger.add(
-            error_log_path,
-            format=_FORMAT_STRING,
-            level=settings.ERROR_LOGGING_LEVEL,
+        file_loggers["error"] = taf_logger.add(
+            error_log_path, format=_FORMAT_STRING, level=settings.ERROR_LOGGING_LEVEL
         )
 
 
 def disable_console_logging(remove_error=False):
-   taf_logger.remove(console_loggers['log'])
-   if remove_error:
-       taf_logger.remove(console_loggers['error'])
-   tuf.log.remove_console_handler()
+    taf_logger.remove(console_loggers["log"])
+    if remove_error:
+        taf_logger.remove(console_loggers["error"])
+    tuf.log.remove_console_handler()
