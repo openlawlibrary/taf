dd3219167d11dbffd424ec2c846393c713032bba
Daniel Elero <danixeee@gmail.com>
2020-03-25 20:46:45 +0100

    Tests update

--- taf/tests/test_repository_tool.py
+++ taf/tests/test_repository_tool.py
@@ -3,13 +3,14 @@ from pathlib import Path
 
 import pytest
 import securesystemslib
-import taf.exceptions
 import tuf
-from taf.tests import TEST_WITH_REAL_YK
-from taf.utils import to_tuf_datetime_format
+
+import taf.exceptions
 import taf.yubikey as yk
 from taf.constants import DEFAULT_RSA_SIGNATURE_SCHEME
+from taf.tests import TEST_WITH_REAL_YK
 from taf.tests.yubikey_utils import VALID_PIN
+from taf.utils import to_tuf_datetime_format
 
 
 @pytest.mark.skipif(TEST_WITH_REAL_YK, reason="Testing with real Yubikey.")
@@ -90,25 +91,37 @@ def test_update_timestamp_wrong_key(repositories, snapshot_key):
 
 def test_update_targets_from_keystore_valid_key(repositories, targets_key):
     taf_happy_path = repositories["test-happy-path"]
-    start_date = datetime.datetime.now()
-    interval = 1
-    expected_expiration_date = to_tuf_datetime_format(start_date, interval)
+    targets_path = Path(taf_happy_path.targets_path)
+    repositories_json_path = targets_path / "repositories.json"
+
+    branch_id = "14e81cd1-0050-43aa-9e2c-e34fffa6f517"
+    target_commit_sha = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    repositories_json_old = repositories_json_path.read_text()
+
+    targets_data = {
+        "branch": {"target": branch_id},
+        "dummy/target_dummy_repo": {"target": {"commit": target_commit_sha}},
+        "capstone": {},
+    }
 
     taf_happy_path.update_targets_keystores(
-        [targets_key], start_date=start_date, interval=interval
+        [targets_key],
+        added_targets_data=targets_data,
+        start_date=datetime.datetime.now(),
     )
-    new_targets_data = str(Path(taf_happy_path.metadata_path) / "targets.json")
-    signable = securesystemslib.util.load_json_file(new_targets_data)
-    tuf.formats.SIGNABLE_SCHEMA.check_match(signable)
-    actual_expiration_date = signable["signed"]["expires"]
 
-    assert actual_expiration_date == expected_expiration_date
+    assert (targets_path / "branch").read_text() == branch_id
+    assert target_commit_sha in (targets_path / "dummy/target_dummy_repo").read_text()
+    assert (targets_path / "capstone").is_file()
+    assert repositories_json_old == repositories_json_path.read_text()
 
 
 def test_update_targets_from_keystore_wrong_key(repositories, snapshot_key):
     taf_happy_path = repositories["test-happy-path"]
-    with pytest.raises(taf.exceptions.InvalidKeyError):
-        taf_happy_path.update_targets_keystores([snapshot_key])
+    targets_data = {"capstone": {}}
+
+    with pytest.raises(taf.exceptions.TargetsMetadataUpdateError):
+        taf_happy_path.update_targets_keystores([snapshot_key], targets_data)
 
 
 def test_update_targets_valid_key_valid_pin(repositories, targets_yk):
@@ -131,7 +144,9 @@ def test_update_targets_valid_key_valid_pin(repositories, targets_yk):
     targets_yk.insert()
     public_key = targets_yk.tuf_key
     taf_happy_path.update_targets_yubikeys(
-        [public_key], targets_data, datetime.datetime.now()
+        [public_key],
+        added_targets_data=targets_data,
+        start_date=datetime.datetime.now(),
     )
 
     assert (targets_path / "branch").read_text() == branch_id
@@ -140,10 +155,48 @@ def test_update_targets_valid_key_valid_pin(repositories, targets_yk):
     assert repositories_json_old == repositories_json_path.read_text()
 
 
+def test_delete_target_file_valid_key_valid_pin(repositories, targets_yk):
+    taf_happy_path = repositories["test-happy-path"]
+    if targets_yk.scheme != DEFAULT_RSA_SIGNATURE_SCHEME:
+        pytest.skip()
+    targets_path = Path(taf_happy_path.targets_path)
+
+    yk.add_key_pin(targets_yk.serial, VALID_PIN)
+    targets_yk.insert()
+    public_key = targets_yk.tuf_key
+
+    # add capstone
+    targets_data = {"capstone": {}}
+    taf_happy_path.update_targets_yubikeys(
+        [public_key],
+        added_targets_data=targets_data,
+        start_date=datetime.datetime.now(),
+    )
+
+    assert (targets_path / "capstone").is_file()
+    targets_obj = taf_happy_path._role_obj("targets")
+    assert "capstone" in targets_obj.target_files
+
+    # remove capstone
+    taf_happy_path.update_targets_yubikeys(
+        [public_key],
+        removed_targets_data=targets_data,
+        start_date=datetime.datetime.now(),
+    )
+
+    assert not (targets_path / "capstone").is_file()
+    targets_obj = taf_happy_path._role_obj("targets")
+    assert "capstone" not in targets_obj.target_files
+
+
 @pytest.mark.skipif(TEST_WITH_REAL_YK, reason="Testing with real Yubikey.")
 def test_update_targets_wrong_key(repositories, root1_yk):
     taf_happy_path = repositories["test-happy-path"]
-    with pytest.raises(taf.exceptions.InvalidKeyError):
+    targets_data = {"capstone": {}}
+
+    with pytest.raises(taf.exceptions.TargetsMetadataUpdateError):
         root1_yk.insert()
         yk.add_key_pin(root1_yk.serial, VALID_PIN)
-        taf_happy_path.update_targets_yubikeys([root1_yk.tuf_key])
+        taf_happy_path.update_targets_yubikeys(
+            [root1_yk.tuf_key], added_targets_data=targets_data
+        )
