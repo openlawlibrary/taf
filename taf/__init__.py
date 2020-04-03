de7e87647de7f1fee9326568d3d7d03232933900
Daniel Elero <danixeee@gmail.com>
2020-04-03 13:47:20 +0200

    Add tuf patches

--- taf/__init__.py
+++ taf/__init__.py
@@ -4,13 +4,6 @@ import os
 import sys
 from pathlib import Path
 
-import tuf.repository_tool
-
-tuf.repository_tool.METADATA_STAGED_DIRECTORY_NAME = (
-    tuf.repository_tool.METADATA_DIRECTORY_NAME
-)
-
-
 _PLATFORM = sys.platform
 
 _PLATFORM_LIBS = str((Path(__file__).parent / "libs").resolve())
@@ -31,3 +24,32 @@ elif _PLATFORM == "win32":
     _set_env("PATH", _PLATFORM_LIBS)
 else:
     raise Exception(f'Platform "{_PLATFORM}" is not supported!')
+
+
+def _tuf_patches():
+    from functools import wraps
+    import tuf.repository_lib
+    import tuf.repository_tool
+
+    from taf.utils import normalize_file_line_endings
+
+    # Replace staging metadata directory name
+    tuf.repository_tool.METADATA_STAGED_DIRECTORY_NAME = (
+        tuf.repository_tool.METADATA_DIRECTORY_NAME
+    )
+
+    # Replace get_metadata_fileinfo with file-endings normalization
+    def get_metadata_fileinfo(get_metadata_fileinfo_fn):
+        @wraps(get_metadata_fileinfo_fn)
+        def normalized(filename, custom=None):
+            normalize_file_line_endings(filename)
+            return get_metadata_fileinfo_fn(filename, custom=None)
+
+        return normalized
+
+    tuf.repository_lib.get_metadata_fileinfo = get_metadata_fileinfo(
+        tuf.repository_lib.get_metadata_fileinfo
+    )
+
+
+_tuf_patches()
