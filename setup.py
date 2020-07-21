0d917d15d2b0fcdac2e283fdbb9284aca2530dfa
danixeee <danixeee@gmail.com>
2020-07-21 13:31:40 +0200

    Release v0.5.2 (#143)
    * Bump version to 0.5.2
    
    * Update CHANGELOG

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 PACKAGE_NAME = "taf"
-VERSION = "0.5.1"
+VERSION = "0.5.2"
 AUTHOR = "Open Law Library"
 AUTHOR_EMAIL = "info@openlawlib.org"
 DESCRIPTION = "Implementation of archival authentication"
