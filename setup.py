4b72acd4aee39a9031e74a8dfe6fe3cb3e4b41a5
Renata <rvaderna@openlawlib.org>
2021-06-29 18:56:44 +0200

    Decrease cryptography - avoid needing openssl

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     install_requires=[
         "click==6.7",
         "colorama>=0.3.9",
-        "cryptography==3.3.1",
+        "cryptography==3.2.1",
         "oll-tuf==0.11.2.dev9",
         "loguru==0.4.0",
     ],
