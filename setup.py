bbffbb95180e37aa8209133fa26df3af8b74508d
Renata <rvaderna@openlawlib.org>
2021-10-19 20:14:45 +0200

    Formatting

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
         "colorama>=0.3.9",
         "cryptography>=3.3",
         "oll-tuf==0.11.2.dev9",
-        "loguru==0.4.0"
+        "loguru==0.4.0",
     ],
     extras_require={
         "ci": ci_require,
