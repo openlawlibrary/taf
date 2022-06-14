d5052bb6e98635659de3d1ab24ee3dab5258561d
Renata Vaderna <rvaderna@openlawlib.org>
2022-06-14 15:45:41 +0200

    Update changelog and bump version (#255)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 from importlib.util import find_spec
 
 PACKAGE_NAME = "taf"
-VERSION = "0.18.0"
+VERSION = "0.19.0"
 AUTHOR = "Open Law Library"
 AUTHOR_EMAIL = "info@openlawlib.org"
 DESCRIPTION = "Implementation of archival authentication"
