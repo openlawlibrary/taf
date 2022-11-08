802217cd6fea049153b4cd2fb96bb6cd9a6fb59b
Renata <rvaderna@openlawlib.org>
2022-11-09 00:24:32 +0100

    fix: remove all_fetched_commits which does not always work
    Instead of all_fetched_commits, all_commits_on_branch, which
    was implemented using pygit2 can be called - as long as the
    branch which is passed to it is a remote branch.
    all_fetched_commits would return an empty list in some cases when
    there were commits on the remote branch. all_commits_on_branch returned
    the correct commits in those cases on the other hand.

--- taf/updater/updater.py
+++ taf/updater/updater.py
@@ -998,7 +998,7 @@ def _get_commits(
 
     if old_head is not None:
         if not only_validate:
-            fetched_commits = repository.all_fetched_commits(branch=branch)
+            fetched_commits = repository.all_commits_on_branch(branch=f"origin/{branch}")
 
             # if the local branch does not exist (the branch was not checked out locally)
             # fetched commits will include already validated commits
@@ -1031,7 +1031,7 @@ def _get_commits(
             new_commits_on_repo_branch = []
         if not only_validate:
             try:
-                fetched_commits = repository.all_fetched_commits(branch=branch)
+                fetched_commits = repository.all_commits_on_branch(branch=f"origin/{branch}")
                 # if the local branch does not exist (the branch was not checked out locally)
                 # fetched commits will include already validated commits
                 # check which commits are newer that the previous head commit
