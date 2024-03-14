10c57b8892d0a10af7182699d5dfc21fe8f548b8
n-dusan <nikolic.dusan.dey@gmail.com>
2024-03-14 16:03:57 +0100

    chore: point release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 from importlib.util import find_spec
 
 PACKAGE_NAME = "taf"
-VERSION = "0.29.1"
+VERSION = "0.29.2"
 AUTHOR = "Open Law Library"
 AUTHOR_EMAIL = "info@openlawlib.org"
 DESCRIPTION = "Implementation of archival authentication"
