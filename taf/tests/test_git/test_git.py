f35dd72c1f77821870f2d73e39b1c6a9164e1575
Renata <rvaderna@openlawlib.org>
2024-04-02 05:04:18 +0200

    chore: remove unused imports

--- taf/tests/test_git/test_git.py
+++ taf/tests/test_git/test_git.py
@@ -1,7 +1,3 @@
-from taf.git import GitRepository
-from taf.tests.test_git.conftest import TEST_DIR, CLONE_REPO_NAME
-
-
 def test_clone_from_local(repository, clone_repository):
     clone_repository.clone_from_disk(repository.path)
     assert clone_repository.is_git_repository
