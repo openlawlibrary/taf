c97897526c6a08581d350d34e52d4f95db452ac0
Renata <rvaderna@openlawlib.org>
2024-06-15 18:48:37 -0400

    devops: drop Python 3.7 support

--- setup.py
+++ setup.py
@@ -103,7 +103,6 @@ kwargs = {
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
