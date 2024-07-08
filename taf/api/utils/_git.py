014e85840fa7c0b606269e3ee9a84bbe9d584362
Ronan Takizawa <71115970+ronantakizawa@users.noreply.github.com>
2024-07-09 08:00:55 +0900

    feat: enhanced commit_and_push (#469)
    * feat: enhanced commit_and_push
    
    * fix: updated CHANGELOG.md
    
    * fix: fixed import errors
    
    ---------
    
    Co-authored-by: Renata Vaderna <rvaderna@openlawlib.org>

--- taf/api/utils/_git.py
+++ taf/api/utils/_git.py
@@ -1,9 +1,7 @@
 import functools
-from logging import ERROR
 from typing import Optional
-from logdecorator import log_on_error
 from taf.auth_repo import AuthenticationRepository
-from taf.exceptions import RepositoryNotCleanError, TAFError
+from taf.exceptions import GitError, RepositoryNotCleanError, TAFError
 from taf.git import GitRepository
 from taf.log import taf_logger
 
@@ -24,13 +22,6 @@ def check_if_clean(func):
     return wrapper
 
 
-@log_on_error(
-    ERROR,
-    "An error occurred while committing and pushing changes: {e}",
-    logger=taf_logger,
-    on_exceptions=TAFError,
-    reraise=True,
-)
 def commit_and_push(
     auth_repo: AuthenticationRepository,
     commit_msg: Optional[str] = None,
@@ -39,6 +30,24 @@ def commit_and_push(
     if commit_msg is None:
         commit_msg = input("\nEnter commit message and press ENTER\n\n")
     auth_repo.commit(commit_msg)
+
     if push and auth_repo.has_remote():
-        auth_repo.push()
-        taf_logger.info("Successfully pushed to remote")
+        try:
+            auth_repo.push()
+            taf_logger.info("Successfully pushed to remote")
+
+            new_commit_branch = auth_repo.default_branch
+            if new_commit_branch:
+                new_commit = auth_repo.top_commit_of_branch(new_commit_branch)
+                if new_commit:
+                    auth_repo.set_last_validated_commit(new_commit)
+                    taf_logger.info(f"Updated last_validated_commit to {new_commit}")
+            else:
+                taf_logger.warning(
+                    "Default branch is None, skipping last_validated_commit update."
+                )
+        except GitError as e:
+            taf_logger.error(
+                f"Push failed: {str(e)}. Please check if there are upstream changes."
+            )
+            raise TAFError("Push operation failed") from e
