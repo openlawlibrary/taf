42fbfac267064fea7d0799f2186b3bb058ed9343
Lukas Puehringer <lukas.puehringer@nyu.edu>
2024-08-28 15:00:12 +0200

    Add signer implementation for Yubikeys
    YkSigner provides a minimal compatibility layer over `taf.yubikey`
    module functions for use with MetadataRepository.
    
    Even though a yubikey signer implementation (HSMSigner) based on
    pykcs11 is available in securesystemslib, YkSigner was added for the
    following reasons:
    
    - TAF requires rsa support for yubikeys, but HSMSigner only supports
      ecdsa. Adding rsa support to HSMSigner, or providing a custom
      pykcs11-based RSAHSMSigner is feasible, and seems desirable, but
      requires more effort than this YkSigner did.
    
    - TAF provides a few additional features, like setting up a Yubikey,
      changing pins, etc., which will not be added to securesystemslib.
      This means the current Yubikey infrastructure based on yubikey-manager
      needs to be preserved for the time being. Thus it made sense to
      re-use the existing implementation for YkSigner.
    
    - YkSigner show-cases the new Signer API and might be used as blue print
      for future Signer implementations in TAF.
    
    This commit adds basic tests with fake and real Yubikey:
    
    ```
    REAL_YK=1 PYTEST_DISABLE_PLUGIN_AUTOLOAD=1 \
        pytest --noconftest  taf/tests/tuf/ taf/tests/tuf/test_yk.py -s
    ```
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- taf/tuf/keys.py
+++ taf/tuf/keys.py
@@ -6,7 +6,13 @@
 from typing import Optional
 
 from pathlib import Path
-from securesystemslib.signer import SSlibKey, CryptoSigner
+from securesystemslib.signer import (
+    SSlibKey,
+    CryptoSigner,
+    Signer,
+    SecretsHandler,
+    Signature,
+)
 from securesystemslib.formats import encode_canonical
 from securesystemslib.hash import digest
 
@@ -15,6 +21,8 @@ from cryptography.hazmat.primitives.serialization import (
     load_pem_public_key,
 )
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey
+
+from taf.yubikey import export_piv_pub_key, sign_piv_rsa_pkcs1v15
 
 
 def _get_legacy_keyid(key: SSlibKey) -> str:
@@ -75,3 +83,63 @@ def load_signer_from_file(path: Path, password: Optional[str]) -> CryptoSigner:
     priv = load_pem_private_key(pem, password)
     pub = priv.public_key()
     return CryptoSigner(priv, _from_crypto(pub))
+
+
+class YkSigner(Signer):
+    """Signer implementation for Yubikeys.
+
+    Provides a minimal compatibility layer over `taf.yubikey` module functions
+    for use with MetadataRepository.
+
+    Attrs:
+        public_key: An SSlibkey, whose keyid and signing scheme are the single
+                of truth for creating signatures.
+        pin_handler: A function, which is called in `sign` and expected to
+                return the Yubikey pin.
+    """
+
+    _SECRET_PROMPT = "pin"
+
+    def __init__(self, public_key: SSlibKey, pin_handler: SecretsHandler):
+
+        self._public_key = public_key
+        self._pin_handler = pin_handler
+
+    @property
+    def public_key(self) -> SSlibKey:
+        return self._public_key
+
+    @classmethod
+    def import_(cls) -> SSlibKey:
+        """Import rsa public key from Yubikey.
+
+        * Assigns default signing scheme: "rsa-pkcs1v15-sha256"
+        * Raises ValueError, if key on Yubikey is not an rsa key.
+
+        TODO: Consider returning priv key uri along with public key.
+        See e.g. `self.from_priv_key_uri` and other `import_` methods on
+        securesystemslib signers, e.g. `HSMSigner.import_`.
+        """
+        # TODO: export pyca/cryptography key to avoid duplicate deserialization
+        pem = export_piv_pub_key()
+        pub = load_pem_public_key(pem)
+        return _from_crypto(pub)
+
+    def sign(self, payload: bytes) -> Signature:
+        pin = self._pin_handler(self._SECRET_PROMPT)
+        # TODO: openlawlibrary/taf#515
+        # sig = sign_piv_rsa_pkcs1v15(payload, pin, self.public_key.keyval["public"])
+        sig = sign_piv_rsa_pkcs1v15(payload, pin)
+        return Signature(self.public_key.keyid, sig.hex())
+
+    @classmethod
+    def from_priv_key_uri(
+        cls,
+        priv_key_uri: str,
+        public_key: SSlibKey,
+        secrets_handler: Optional[SecretsHandler] = None,
+    ) -> "Signer":
+        # TODO: Implement this to better separate public key management
+        # (e.g. tuf delegation) and signer configuration from signing. See
+        # https://python-securesystemslib.readthedocs.io/en/latest/signer.html
+        raise NotImplementedError
