0789b34ad738ed79f83bfc764d56c7e0d03e1ef7
n-dusan <nikolic.dusan.dey@gmail.com>
2024-09-19 00:22:53 +0000

    fchore: formatting

--- setup.py
+++ setup.py
@@ -24,11 +24,7 @@ ci_require = [
     "freezegun==0.3.15",
 ]
 
-executable_require = [
-    "lxml",
-    "requests",
-    "rdflib"
-]
+executable_require = ["lxml", "requests", "rdflib"]
 
 dev_require = ["bandit>=1.6.0", "black>=19.3b0", "pre-commit>=1.18.3"]
 
