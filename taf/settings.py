c191b58c855644003253740d8ccd6f180134ae84
n-dusan <nikolic.dusan.dey@gmail.com>
2024-09-20 22:06:39 +0000

    chore(revert): turn on script from local disk for testing

--- taf/settings.py
+++ taf/settings.py
@@ -27,7 +27,7 @@ overwrite_last_validated_commit = False
 last_validated_commit: dict = {}
 
 # determines if script files will be loaded from disk
-development_mode = False
+development_mode = True
 
 # The 'log.py' module manages TUF's logging system.  Users have the option to
 # enable/disable logging to a file via 'ENABLE_FILE_LOGGING', or
