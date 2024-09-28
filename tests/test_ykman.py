9819b8f757ee92ea1d16ffe340b4386fb92ceba1
Yukta Kulkarni <yuktakulkarni@Yuktas-MBP.lan>
2024-09-28 13:48:25 -0400

    Updated taf.yubikey for issue #501

--- tests/test_ykman.py
+++ tests/test_ykman.py
@@ -1,12 +1,13 @@
 import pytest
+
 try:
-    from ykman.device import list_all_devices
-    YKMAN_AVAILABLE = True
+    import taf.yubikey as yk
+    YKMAN_AVAILABLE = yk.YKMAN_AVAILABLE
 except ImportError:
     YKMAN_AVAILABLE = False
 
-@pytest.mark.skipif(not YKMAN_AVAILABLE, reason="ykman module is not available")
-def test_some_function_that_uses_ykman():
-    pass
+@pytest.mark.skipif(not YKMAN_AVAILABLE, reason="taf.yubikey or ykman module is not available")
+def test_repository_tool_function_that_uses_yubikey():
+    assert yk.some_function_using_ykman() is not None
 
 pytest
