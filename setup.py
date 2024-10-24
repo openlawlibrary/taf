5fc605be3acf20804a70b0f71c50adaeb167c7aa
Renata <rvaderna@openlawlib.org>
2024-10-23 20:03:34 -0400

    chore: update changelog and seutp.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 PACKAGE_NAME = "taf"
-VERSION = "0.31.2"
+VERSION = "0.32.0"
 AUTHOR = "Open Law Library"
 AUTHOR_EMAIL = "info@openlawlib.org"
 DESCRIPTION = "Implementation of archival authentication"
