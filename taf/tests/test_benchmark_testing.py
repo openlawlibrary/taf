79507b4206a929290264885fb963e3e08d792b31
CalShucha <callenshucha@gmail.com>
2025-01-21 12:51:02 -0600

    Black formatting

--- taf/tests/test_benchmark_testing.py
+++ taf/tests/test_benchmark_testing.py
@@ -3,9 +3,11 @@ import pytest
 import pytest_benchmark
 import random
 
+
 def test_benchmark(benchmark):
     benchmark(get_useless_number)
-    #benchmark(time.sleep, 0.6)
+    # benchmark(time.sleep, 0.6)
+
 
 def test_concurrency_with_file_writing(benchmark):
     benchmark(write_numbered_lines)
@@ -17,15 +19,18 @@ def test_benchmark_two(benchmark):
 
 def function_test():
     time.sleep(0.1)
-    #assert False
+    # assert False
 
 
 def write_numbered_lines():
     for i in range(2):
-        with open("C:\\Users\\doxie\\OneDrive\\Desktop\\TestOutput.txt", 'a') as the_file:
+        with open(
+            "C:\\Users\\doxie\\OneDrive\\Desktop\\TestOutput.txt", "a"
+        ) as the_file:
             the_file.write(f"Line {i}\n")
             the_file.close()
         time.sleep(random.random() / 1000)
+
 
 def get_useless_number():
     try:
