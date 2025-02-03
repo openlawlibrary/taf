48561c15fa0e7f5974d2f6ac3bcbb9643c517b04
CalShucha <callenshucha@gmail.com>
2025-02-03 11:39:17 -0600

    Disabled benchmark test

--- taf/tests/test_updater/test_update_benchmarking.py
+++ taf/tests/test_updater/test_update_benchmarking.py
@@ -3,7 +3,7 @@ from taf.tests.test_updater.test_clone.test_clone_valid import (
     test_clone_valid_happy_path,
 )
 
-
+@pytest.mark.skip(reason="benchmarking disabled for time being")
 @pytest.mark.parametrize(
     "origin_auth_repo",
     [
