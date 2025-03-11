6f2bb74fbb743ee01a706ad20a1e443056f550af
Renata <rvaderna@openlawlib.org>
2025-03-11 18:07:24 -0400

    fix: optionally include yubikey cli group

--- taf/tools/cli/taf.py
+++ taf/tools/cli/taf.py
@@ -2,21 +2,46 @@ import click
 from taf.tools.cli.lazy_group import LazyGroup
 
 
-@click.group(cls=LazyGroup, lazy_subcommands={
-    "dependencies": "taf.tools.dependencies.attach_to_group",
-    "keystore": "taf.tools.keystore.attach_to_group",
-    "conf": "taf.tools.conf.attach_to_group",
-    "repo": "taf.tools.repo.attach_to_group",
-    "targets": "taf.tools.targets.attach_to_group",
-    "metadata": "taf.tools.metadata.attach_to_group",
-    "roles": "taf.tools.roles.attach_to_group",
-    "yubikey": "taf.tools.yubikey.attach_to_group",
-})
+def taf_command_loader(name):
+    if name != "yubikey":
+        if name in (
+            "dependencies",
+            "keystore",
+            "conf",
+            "targets",
+            "metadata",
+            "roles",
+            "repo",
+        ):
+            return f"taf.tools.{name}.attach_to_group"
+        else:
+            raise RuntimeError(f"Unknown command: {name}")
+    try:
+        import ykman
+
+        return f"taf.tools.yubikey.attach_to_group"
+    except ImportError:
+        pass
+
+
+@click.group(
+    cls=LazyGroup,
+    lazy_subcommands={
+        "dependencies": lambda: taf_command_loader("dependencies"),
+        "keystore": lambda: taf_command_loader("keystore"),
+        "conf": lambda: taf_command_loader("conf"),
+        "repo": lambda: taf_command_loader("repo"),
+        "targets": lambda: taf_command_loader("targets"),
+        "metadata": lambda: taf_command_loader("metadata"),
+        "roles": lambda: taf_command_loader("roles"),
+        "yubikey": lambda: taf_command_loader("yubikey"),
+    },
+)
 @click.version_option()
 def taf():
     """TAF Command Line Interface"""
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     taf()
