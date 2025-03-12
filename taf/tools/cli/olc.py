45fb71c66a54fd3ba6341284b3b848f0abec7bf2
Renata Vaderna <rvaderna@openlawlib.org>
2025-03-12 10:56:16 -0400

    fix: optionally include yubikey cli group (#604)
    * fix: optionally include yubikey cli group
    
    * docs: update changelog
    
    * chore: flake fixes

--- taf/tools/cli/olc.py
+++ taf/tools/cli/olc.py
@@ -2,14 +2,12 @@ import click
 from taf.tools.cli.lazy_group import LazyGroup
 
 
-@click.group(cls=LazyGroup, lazy_subcommands={
-    "repo": "taf.tools.repo.attach_to_group"
-})
+@click.group(cls=LazyGroup, lazy_subcommands={"repo": "taf.tools.repo.attach_to_group"})
 @click.version_option()
 def olc():
     """OLC Command Line Interface"""
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     olc()
