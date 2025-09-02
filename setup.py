e7ef8a982c93fa7941e875357b15bd00f2db9caf
n-dusan <nikolic.dusan.dey@gmail.com>
2025-09-02 13:29:00 +0200

    chore: bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 PACKAGE_NAME = "taf"
-VERSION = "0.35.4"
+VERSION = "0.35.5"
 AUTHOR = "Open Law Library"
 AUTHOR_EMAIL = "info@openlawlib.org"
 DESCRIPTION = "Implementation of archival authentication"
