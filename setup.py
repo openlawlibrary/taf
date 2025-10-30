295668e81a05ad733e5343446a67f7b3371bbd86
n-dusan <nikolic.dusan.dey@gmail.com>
2025-10-30 16:57:58 +0100

    chore: prerelease and changelog

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 PACKAGE_NAME = "taf"
-VERSION = "0.35.5"
+VERSION = "0.36.0a1"
 AUTHOR = "Open Law Library"
 AUTHOR_EMAIL = "info@openlawlib.org"
 DESCRIPTION = "Implementation of archival authentication"
