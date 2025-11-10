2d34eec40916c2c810f3e75416c72a93eeafb828
Renata <rvaderna@openlawlib.org>
2025-11-10 16:52:44 +0100

    feat: add skip-keygen option to taf init command
    If it is provided, the user will not be asked if they want to generate a new keystore.
    Useful for integration with other tools.

--- taf/tools/conf/__init__.py
+++ taf/tools/conf/__init__.py
@@ -7,8 +7,9 @@ def init_command():
     @click.option("--path", default=".", help=".taf directory's location. If not specified, set to the current directory")
     @click.option("--keystore", default=None, help="Location of the keystore directory to copy from. Can be specified in keys-description dictionary")
     @click.option("--keys-description", help="A dictionary containing information about the keys or a path to a json file which stores the needed information")
-    def _init_command(path, keystore, keys_description):
-        init(path, keystore, keys_description)
+    @click.option("--skip-keygen", is_flag=True, default=False, help="Skip generation of keys even if they do not exist")
+    def _init_command(path, keystore, keys_description, skip_keygen):
+        init(path, keystore, keys_description, skip_keygen)
     return _init_command
 
 
